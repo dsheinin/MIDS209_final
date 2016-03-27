6a2995f60bfd541f8b9620c0676f78c4a035339b
Daniel Sheinin <dsheinin@gmail.com>
2016-03-27 00:40:09 -0700

    Modify API to suit D3 and add test multi-line graph

--- django/w209final/apps/core/views.py
+++ django/w209final/apps/core/views.py
@@ -3,23 +3,47 @@ from django.http import JsonResponse
 
 def fetch_coverage(request, group_slug):
 
-    country_data = []
-    for country in Country.objects.all():
-        vaccine_data = []
-        for vaccine in Vaccine.objects.filter(group__slug=group_slug):
-            coverage_data = []
-            for vac_estimate in VaccinationEstimate.objects.filter(country=country, vaccine=vaccine):
-                coverage_data.append({
-                    'year':vac_estimate.year,
-                    'coverage':vac_estimate.coverage,
-                })
-            vaccine_data.append({
-                'vaccine':vaccine.code,
-                'coverage_by_year':coverage_data,
-            })
-        country_data.append({
-            'name':country.name,
-            'vaccines':vaccine_data,
-        })
+    """
+    Returns JSON coverage data in the following format:
+    {
+        'years': [1984, 1985, 1986],
+        'countries': [
+            {
+                'name':'Algeria',
+                'years': [
+                    {
+                        'year': 1984,
+                        'coverage': {
+                            'DTP1': None,
+                            'DTP3': 47
+                        }
+                    },
+                ]
+            }
+        ]
+    }
+    """
 
-    return JsonResponse({'countries':country_data})
+    estimate_qs = VaccinationEstimate.objects.filter(vaccine__group__slug=group_slug).order_by('year')
+    years = list(estimate_qs.values_list('year', flat=True).distinct())
+    countries = []
+
+    for country in Country.objects.all():
+
+        # This is a straightforward way of getting the data, but slow--1 DB query per year per country
+        #country_data = {'name': country.name, 'years':[]}
+        #for year in years:
+        #    year_data = {'year':year, 'coverage':{}}
+        #    for estimate in estimate_qs.filter(country=country, year=year):
+        #        year_data['coverage'][estimate.vaccine.code] = estimate.coverage
+        #    country_data['years'].append(year_data)
+
+        # This is faster--ony 1 DB query per country. Requires that estimates and years be ordered by year
+        country_data = {'name': country.name, 'years':[{'year':year, 'coverage':{}} for year in years]}
+        for estimate in estimate_qs.filter(country=country):
+            country_data['years'][years.index(estimate.year)]['coverage'][estimate.vaccine.code] = estimate.coverage
+
+        countries.append(country_data)
+
+    return JsonResponse({'years':years, 'countries':countries})
+
