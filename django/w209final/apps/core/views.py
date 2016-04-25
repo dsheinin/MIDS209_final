908697cfe690905330b63d8bf647984d87b9cd12
Daniel Sheinin <dsheinin@gmail.com>
2016-04-24 19:21:26 -0700

    Fix bug: firefox js error (don't return coverage record for country with no data)

--- django/w209final/apps/core/views.py
+++ django/w209final/apps/core/views.py
@@ -125,35 +125,37 @@ def fetch_coverage(request, group_slug):
 
     for country in Country.objects.all().order_by('name'):
 
-        # This is a straightforward way of getting the data, but slow--1 DB query per year per country
-        #country_data = {'name': country.name, 'years':[]}
-        #for year in years:
-        #    year_data = {'year':year, 'coverage':{}}
-        #    for estimate in estimate_qs.filter(country=country, year=year):
-        #        year_data['coverage'][estimate.vaccine.code] = estimate.coverage
-        #    country_data['years'].append(year_data)
+        if estimate_qs.filter(country=country).exists():
 
-        # This is faster--ony 1 DB query per country. Requires that estimates and years be ordered by year
-        country_data = {
-            'name': country.name,
-            'iso_code': country.iso_code,
-            'years':[{'year':year, 'coverage':{}} for year in years]
-        }
-        for estimate in estimate_qs.filter(country=country):
-            vac_code = estimate.vaccine.code
-            year_index = years.index(estimate.year)
-            # add estimate to date for this country
-            country_data['years'][year_index]['coverage'][vac_code] = estimate.coverage
+            # This is a straightforward way of getting the data, but slow--1 DB query per year per country
+            #country_data = {'name': country.name, 'years':[]}
+            #for year in years:
+            #    year_data = {'year':year, 'coverage':{}}
+            #    for estimate in estimate_qs.filter(country=country, year=year):
+            #        year_data['coverage'][estimate.vaccine.code] = estimate.coverage
+            #    country_data['years'].append(year_data)
 
-            # add to average data
-            if vac_code not in average_years[year_index]['coverage']:
-                # store averages as (total, count) tuples while incrementing
-                average_years[year_index]['coverage'][vac_code] = (estimate.coverage, 1)
-            else:
-                total, count = average_years[year_index]['coverage'][vac_code]
-                average_years[year_index]['coverage'][vac_code] = (total + estimate.coverage, count + 1)
+            # This is faster--ony 1 DB query per country. Requires that estimates and years be ordered by year
+            country_data = {
+                'name': country.name,
+                'iso_code': country.iso_code,
+                'years':[{'year':year, 'coverage':{}} for year in years]
+            }
+            for estimate in estimate_qs.filter(country=country):
+                vac_code = estimate.vaccine.code
+                year_index = years.index(estimate.year)
+                # add estimate to date for this country
+                country_data['years'][year_index]['coverage'][vac_code] = estimate.coverage
 
-        countries.append(country_data)
+                # add to average data
+                if vac_code not in average_years[year_index]['coverage']:
+                    # store averages as (total, count) tuples while incrementing
+                    average_years[year_index]['coverage'][vac_code] = (estimate.coverage, 1)
+                else:
+                    total, count = average_years[year_index]['coverage'][vac_code]
+                    average_years[year_index]['coverage'][vac_code] = (total + estimate.coverage, count + 1)
+
+            countries.append(country_data)
 
     # calculate averages from (total, count) tuples
     for year_avg in average_years:
